fc3a7105c98c50117a210ecf69cdcc6bc9deca4f
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-02-18 22:14:46 +0200

    mainSuckess

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '25738191-2a68a887d2690264ace752ae1'
+const KEY = '25738191-2a68a887d2690264ace752ae1';
 
-async function getPictures (name) {
-     const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo`);
-    const data = await response.data;
-
+async function getPictures (name, page) {
+    const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+    try {
+        const data = await response.data;
         return data;
-}
+    }
+    catch (error) {
+    }
+}    
 
 export default { getPictures };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,67 @@
 import './css/common.css'
 import API from './api/fetch';
 import RENDER from './components/render'
+import Notiflix from 'notiflix';
 
 const render = document.querySelector('.gallery');
 const inputValue = document.querySelector('.search-form');
+const clear = '';
+const loadMore = document.querySelector('.load-more');
+let name = '';
+let page = 1;
 
 inputValue.addEventListener('submit', search);
+loadMore.hidden = true;
+loadMore.addEventListener('click', onLoadMore);
 
 function search(e) {
     e.preventDefault();
-    const name = e.currentTarget.elements.searchQuery.value.trim();
-    console.log(name);
-    API.getPictures(name)
-        .then(renderPicture)
+    loadMore.hidden = false;
+    loadMore.disabled = true;
+   
+        name = e.currentTarget.elements.searchQuery.value.trim();
+        clearRender();
+        if (name.length === 0) {
+            return;
+        }
+        API.getPictures(name, page)
+            .then(renderPicture)
+            .catch(onFetchError)
+    inputValue.reset();
+    }
+
+function clearRender() {
+    render.innerHTML = clear;
+    page = 1;
 }
 
 function renderPicture(picture) {
-    const markup = RENDER(picture.hits);
-    console.log(picture);
-    render.insertAdjacentHTML('beforeend', markup);
+    const totalPics = picture.totalHits / (page * picture.hits.length);
+    console.log(totalPics);
+    if (picture.hits.length === 0) {
+        onFetchError();
     }
+    if (totalPics < 1) {
+        Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
+        loadMore.hidden = true;
+    }
+    const markup = RENDER(picture.hits);
+    render.insertAdjacentHTML('beforeend', markup);
+    Notiflix.Notify.success(`Hooray! We found ${picture.totalHits} images.`);
+    loadMore.disabled = false;    
+}
+    
+function onFetchError() {
+    Notiflix.Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+    render.innerHTML = clear;
+}
+
+function onLoadMore() {
+    loadMore.disabled = true;
+    page += 1;
+    API.getPictures(name, page)
+            .then(renderPicture)
+        .catch(onFetchError)
+    loadMore.disabled = false;
+    
+}
